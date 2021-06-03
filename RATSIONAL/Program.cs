73f038c86911e7c21b9a165be2828f467763e515
KamolHasanov <khasanovkamol3834@gmail.com>
2021-06-03 08:25:14 +0500

    Add project files.

---
+++ RATSIONAL/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RATSIONAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ratsional[] ratsionals = new Ratsional[3];
+            ratsionals[0] = new Ratsional(12, 4);
+            ratsionals[1] = new Ratsional(24, 6);
+            ratsionals[2] = new Ratsional();
+            //1-masala: Berilgan a va b ratsional sonlar o'zaro tengmi?
+            if(ratsionals[2].Tekshir(ratsionals[0], ratsionals[1]))
+            {
+                Console.WriteLine("Ushbu ikkita ratsional son o'zaro teng");
+            }
+            else
+            {
+                Console.WriteLine("Ushbu ikkita ratsional son o'zaro teng emas");
+            }
+            //2-masala: Berilgan a va b ratsional sonni yig'indisini toping
+            ratsionals[2] = ratsionals[2].Sum(ratsionals[0], ratsionals[1]);
+            Console.WriteLine(ratsionals[2].surat+"/"+ratsionals[2].maxraj);
+
+            //3-masala: Berilgan a va b ratsional sonni qisqartring:
+            Console.WriteLine("Berilgan a va b ratsional sonni qisqardi");
+           ratsionals[2]=ratsionals[2].Qisqartirish(ratsionals[2]);
+            Console.WriteLine(ratsionals[2].surat+"/"+ratsionals[2].maxraj);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ RATSIONAL/Ratsional.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RATSIONAL
+{
+    public class Ratsional
+    {
+        public int surat { get; set; }
+        public int maxraj { get; set; }
+        public Ratsional()
+        {
+
+        }
+        public  Ratsional(int _surat,int _maxraj)
+        {
+            this.surat = _surat;
+            this.maxraj = _maxraj;
+        }
+        //Berilgan a va b ratsional sonni tekshirish
+        public bool Tekshir(Ratsional ratsional1,Ratsional ratsional2)
+        {
+            this.surat = ratsional1.surat * ratsional2.maxraj;
+            this.maxraj = ratsional1.maxraj * ratsional2.surat;
+
+            if (this.surat
+                ==this.maxraj) return true;
+            return false;
+        }
+
+        // Berilgan a va b ratsional sonni yig'indisi
+        public Ratsional Sum(Ratsional ratsional1, Ratsional ratsional2)
+        {
+            maxraj = ratsional1.maxraj * ratsional2.maxraj;
+            surat = ratsional2.maxraj * ratsional1.surat + ratsional1.maxraj * ratsional2.surat;
+            return this;
+        }
+    
+        // Berilgan a va b ratsional sonni qisqartirish
+        public Ratsional Qisqartirish(Ratsional ratsional)
+        {
+            this.surat = ratsional.surat;
+            this.maxraj = ratsional.maxraj;
+            for (int i = 2; i < surat/2; i++)
+            {
+                if (surat%i==0 && maxraj%i==0)
+                {
+                    surat=surat / i;
+                    maxraj=maxraj / i;
+                    i--;
+                }
+            }
+            return this;
+        }
+    
+    }
+}
